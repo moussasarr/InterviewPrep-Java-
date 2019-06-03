0c1a4ae3b967447e270ebca2fe8003f570ae3a72
Moussa Sarr <moussasarr490@gmail.com>
2019-06-03 16:45:53 -0400

    first working 100% solution for LeetCode 3, longest-substring-without-repeating-characters

--- LongestNonRepeatingSubstring.java
+++ LongestNonRepeatingSubstring.java
@@ -4,32 +4,36 @@ class LongestNonRepeatingSubstring {
 
   public static int maxSubLength(String s){
     if((s == null) || (s == "") || (s.equals(new String("")))){
-          return 0;
-        }
-        HashMap<Character, Integer> chars;
+         return 0;
+       }
+       HashMap<Character, Integer> chars;
 
-        int sLen = s.length();
-        int longest = 1;
-        for(int i = 0; i < sLen; i++){
-            if( (sLen - i <= longest) && (sLen > 2 )){
-              return longest;
-            }
-            chars = new HashMap<>();
-            chars.put(s.charAt(i), i);
-            for(int j = i + 1; j < sLen; j++){
-                if(chars.get(s.charAt(j)) == null){
-                    chars.put(s.charAt(j), j);
-                    if(sLen == 2){
-                        return 2;
-                    }
-                } else {
-                    longest = Math.max(longest, j - i);
-                    i = chars.get(s.charAt(j));
-                    break;
-                }
-            }
-        }
-        return longest;
+       int sLen = s.length();  //2
+       int longest = 1;
+       for(int i = 0; i < sLen; i++){
+          if(sLen - i <= longest){
+            break;
+          }
+
+           //Put the first char in HashMap
+           chars = new HashMap<>();
+           chars.put(s.charAt(i), i);   //a in hashmap
+           int len = 1;
+           //Looping through
+           for(int j = i + 1; j < sLen; j++){
+               if(chars.get(s.charAt(j)) == null){
+                   chars.put(s.charAt(j), j);
+                   len++; //2
+               } else {
+                   longest = Math.max(longest, len);
+                   i = chars.get(s.charAt(j));
+                   break;
+               }
+           }
+           longest = Math.max(longest, len);
+       }
+       return longest;
+   }
   }
 
   public static void main(String[] args){
